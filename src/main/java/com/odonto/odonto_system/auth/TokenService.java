61c1e6aa00b7a731afb85dc6ec1b5b9f5f051303
Gabriel Mendes <bielmendes2208@gmail.com>
2026-06-12 20:22:53 -0300

    Criando a classe TokenService, responsável gerar um token e validá-lo

---
+++ src/main/java/com/odonto/odonto_system/auth/TokenService.java
@@ -0,0 +1,50 @@
+package com.odonto.odonto_system.auth;
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Service
+public class TokenService {
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    @Value("${api.security.token.expiration}")
+    private Long expiration;
+
+    // Gera um token contendo email e peril do usuário
+    public String genrateToken (String email, String role) {
+        SecretKey key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+
+        return Jwts.builder()
+                .subject(email)
+                .claim("role", role)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(key)
+                .compact();
+    }
+
+    // Valida o token e pega o email
+    public String validateToken (String token) {
+        try {
+            SecretKey key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+
+            return Jwts.parser()
+                    .verifyWith(key)
+                    .build()
+                    .parseSignedClaims(token)
+                    .getPayload()
+                    .getSubject();
+        } catch (JwtException | IllegalArgumentException e) {
+            return null;
+        }
+    }
+
+}
