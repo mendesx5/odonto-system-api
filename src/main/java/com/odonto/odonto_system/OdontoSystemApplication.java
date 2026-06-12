520362a5ddd9fbbfc70e3161aa2ed128421cfeb9
Gabriel Mendes <bielmendes2208@gmail.com>
2026-06-11 22:12:53 -0300

    Criação da entidade base, classe 'BaseEntity'; Organização da estrutura de pacotes feature-based; Criação dos aqruivos de migration (sem conteúdo ainda)

--- src/main/java/com/odonto/odonto_system/OdontoSystemApplication.java
+++ src/main/java/com/odonto/odonto_system/OdontoSystemApplication.java
@@ -2,8 +2,10 @@ package com.odonto.odonto_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class OdontoSystemApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/odonto/odonto_system/shared/BaseEntity.java
@@ -0,0 +1,32 @@
+package com.odonto.odonto_system.shared;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(updatable = false, nullable = false)
+    private UUID id;
+
+    @CreatedDate
+    @Column(name = "created-at", updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated-at", nullable = false)
+    private LocalDateTime updatedAt;
+
+}
