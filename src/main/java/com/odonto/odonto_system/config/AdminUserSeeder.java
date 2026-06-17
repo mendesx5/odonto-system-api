ff55eb7d5f4426fedb28b4e20c8d74de89b99df5
Gabriel Mendes <bielmendes2208@gmail.com>
2026-06-17 14:47:29 -0300

    Aatualização no AdminUserSeeder e .env.example

--- src/main/java/com/odonto/odonto_system/config/AdminUserSeeder.java
+++ src/main/java/com/odonto/odonto_system/config/AdminUserSeeder.java
@@ -3,6 +3,7 @@ package com.odonto.odonto_system.config;
 import com.odonto.odonto_system.user.User;
 import com.odonto.odonto_system.user.UserRepository;
 import com.odonto.odonto_system.user.UserRole;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,15 +12,19 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class AdminUserSeeder {
 
+    @Value("${ADMIN_EMAIL}")
+    private String adminEmail;
+
+    @Value("${ADMIN_PASSWORD}")
+    private String adminPassword;
+
     @Bean
     ApplicationRunner seedAdmin(UserRepository repo, PasswordEncoder encoder) {
         return args -> {
-            String adminEmail = "admin@odonto.com";
-
             if (repo.findByEmail(adminEmail).isEmpty()) {
                 User admin = User.builder()
                         .email(adminEmail)
-                        .password(encoder.encode("ClinicaSegura@2026"))
+                        .password(encoder.encode(adminPassword))
                         .fullname("Gabriel Mendes Admin")
                         .role(UserRole.ADMIN)
                         .active(true)
